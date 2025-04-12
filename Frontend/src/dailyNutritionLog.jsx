528aaeef70a6ae0e32a0b4e10f5b85ae9113a3e8
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2025-04-12 14:35:59 +0530

    gtr

--- Frontend/src/dailyNutritionLog.jsx
+++ Frontend/src/dailyNutritionLog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styles from './dailyNutritionLog.module.css';
+import styles from './DailyNutritionLog.module.css';
 
 const DailyNutritionLog = () => {
   const [nutritionData, setNutritionData] = useState([]);
